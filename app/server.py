a373dd26292f81e6f65a4a79aa63c838961b4411
Pete Humiston <petehumiston@gmail.com>
2020-03-07 19:56:52 -0600

    updated model 3/7/2020

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1A7h1mXrPMQlus41XFyXPj8oD_Qkt3N7'
+export_file_name = 'final_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bull_flag', 'bear_flag']
 path = Path(__file__).parent
 
 app = Starlette()
